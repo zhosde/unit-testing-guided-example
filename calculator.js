dc8cb3c327a259aa4bb0c9aa911435cbffff66b1
Hongshan <zhou.hongshan636@gmail.com>
2021-07-06 11:13:02 +0200

    done

--- calculator.js
+++ calculator.js
@@ -1,14 +1,35 @@
 function sum(a, b) {
-  return;
-}
+  if (typeof a === 'number' && typeof b !== 'number'){
+    return a+0}
+  else if (typeof b !== 'number' && typeof a === 'number'){
+    return b+0
+  }
+  else if (typeof a !== 'number' && typeof b !== 'number') {
+     return 0}
+  else {return a + b};
+    }
+  
+
 
 function subtract(a, b) {
-  return;
+  if (typeof a === 'number' && typeof b !== 'number'){
+    return a-0}
+  else if (typeof b !== 'number' && typeof a === 'number') {
+    return b-0
+  }
+  else if (typeof a !== 'number' && typeof b !== 'number') {
+     return 0}
+  else {return a - b};
 }
 
 function divide(a, b) {
-  return;
-}
+  if (b===0){
+    throw new Error('An explanatory error message')}
+    else {
+      return a / b
+    }
+  }
+
 
 function multiply(a, b) {
   return a * b;
